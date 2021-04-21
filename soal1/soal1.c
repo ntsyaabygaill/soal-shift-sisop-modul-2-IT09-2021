67a77c7ab3ed0d80efe362ff53f8b7c3bda4dc4b
ntsyaabygaill <65441910+ntsyaabygaill@users.noreply.github.com>
2021-04-21 16:53:54 +0700

    Update soal1.c

--- soal1/soal1.c
+++ soal1/soal1.c
@@ -1,222 +1,219 @@
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
+#include <errno.h>
 #include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
+#include <syslog.h>
 #include <string.h>
-#include <wait.h>
 #include <time.h>
-#include <memory.h>
+#include <signal.h>
+#include <wait.h>
 
-// void zipFinal(){
-//     char *argv[] = {"zip", "-r" ,"Lopyu_Stevany.zip","Musyik/","Pyoto/", "Fylm", NULL};
-//     execv("/bin/zip", argv);
-// }
-void delete(){
-char *argv[] = {"rm", "-r" ,"Fylm/","Musyik/","Pyoto/",NULL};
-        execv("/bin/rm", argv);
+int mysignal = 1 ;
+
+void custom_signal_x(int signum) {
+    mysignal = 0 ;
 }
-int main()
+
+void make_program(char b[]) {
+    FILE* src = fopen("killer.sh", "w") ;
+    fputs(b, src) ;
+    fclose(src) ;
+}
+
+char* format_time()
 {
-    int ret,ret2, ret3;
-    time_t date1, date2, date3;
+    char* output;
+    time_t rawtime;
+    struct tm * timeinfo;
 
-    struct tm info;
-    char buffer[80];
+    time ( &rawtime );
+    timeinfo = localtime ( &rawtime );
 
-    info.tm_year = 2001 - 1900;
-    info.tm_mon = 4 - 1;
-    info.tm_mday = 9;
-    info.tm_hour = 16;
-    info.tm_min = 22;
-    info.tm_sec = 0;
-    info.tm_isdst = -1;
+    sprintf(output, "[%d-%d-%d_%d:%d:%d]",timeinfo->tm_year + 1900, timeinfo->tm_mon + 1, timeinfo->tm_mday, timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec);
+    return output;
+}
 
-    ret = mktime(&info);
+int main(int argc,char* argv[]) //supaya dapat passing arguments
+{ 
+  char message[100]="Download Success", ch;
+	int i;
+	
+	for(i = 0; message[i] != '\0'; ++i){
+		ch = message[i];
+		
+		if(ch >= 'a' && ch <= 'z'){
+			ch = ch + 4;
+			
+			if(ch > 'z'){
+				ch = ch - 'z' + 'a' - 1;
+			}
+			
+			message[i] = ch;
+		}
+		else if(ch >= 'A' && ch <= 'Z'){
+			ch = ch + 4;
+			
+			if(ch > 'Z'){
+				ch = ch - 'Z' + 'A' - 1;
+			}
+			
+			message[i] = ch;
+		}
+	}
 
-    /*struct tm infoo;
-    char buff[80];
+  if(argc == 1) // if user doesn't enter any arguments
+  {
+    printf("Enter the program mode! Try running the program again.\n");
+    exit(EXIT_FAILURE);
+  }
 
-    infoo.tm_year = 2021 - 1900;
-    infoo.tm_mon = 4 - 1;
-    infoo.tm_mday = 9;
-    infoo.tm_hour = 22;
-    infoo.tm_min = 22;
-    infoo.tm_sec = 0;
-    infoo.tm_isdst = -1;
-
-    ret3 = mktime(&infoo);*/
-
-    time_t     now;
-    struct tm  ts;
-    char       buf[80];
-
-    // Get current time
-    time(&now);
-
-    // Format time, "ddd yyyy-mm-dd hh:mm:ss zzz"
-    ts = *localtime(&now);
-
-    ret2 = mktime(&ts);
-
-    date1 = ret;
-    date2 = ret2;
-    //date3 = ret3;
-    double seconds = difftime(date1, date2);
-    //double seconds2 = difftime(date3, date2);
-
-    int n1 = fork();
-    int n2 = fork();
-    int status;
-    // pid_t anjay;
-    // anjay = fork();
-    if (n1 == 0 && n2 == 0 && seconds < 0){
-        while ((wait(&status)) > 0);
-
-        pid_t anjes;
-        anjes = fork();
-
-        if (anjes < 0)
-        {
-        exit(EXIT_FAILURE);
+if (argc > 2) {
+        return 0 ;
+    }
+    else if (argc == 2) {
+        char b[80] ;
+        if (!strcmp(argv[1], "-z")) {
+            strcpy(b, "#!/bin/bash\nkillall -9 ./soal3\nrm $0\n") ;
+            make_program(b) ;
         }
-        if (anjes == 0)
-        {
-        char *argv[] = {"wget", "--no-check-certificate", "https://drive.google.com/uc?id=1ZG8nRBRPquhYXq_sISdsVcXx5VdEgi-J&export=download", "-O", "Musik_for_Stevany.zip", NULL};
-        execv("/bin/wget", argv);
+        else if (!strcmp(argv[1], "-x")) {
+            strcpy(b, "#!/bin/bash\nkillall -15 ./soal3\nrm $0\n") ;
+            make_program(b) ;
+            signal(SIGTERM, custom_signal_x) ;
         }
-        else{
-            while ((wait(&status)) > 0);
-            
-            pid_t anjas;
-            anjas = fork();
+        else {
+            return 0 ;
+        }
+    }
+    else {
+        return 0 ;
+    }
 
-            if (anjas < 0)
-            {
+  pid_t pid, sid;        // Variabel untuk menyimpan PID
+
+  pid = fork();     // Menyimpan PID dari Child Process
+
+  /* Keluar saat fork gagal
+  * (nilai variabel pid < 0) */
+  if (pid < 0)
+  {
+    exit(EXIT_FAILURE);
+  }
+
+  /* Keluar saat fork berhasil
+  * (nilai variabel pid adalah PID dari child process) */
+  if (pid > 0)
+  {
+    exit(EXIT_SUCCESS);
+  }
+
+  umask(0);
+
+  sid = setsid();
+  if (sid < 0)
+  {
+    exit(EXIT_FAILURE);
+  }
+
+  char cwd[100];
+  getcwd(cwd, sizeof(cwd));
+
+  if ((chdir("/")) < 0)
+  {
+    exit(EXIT_FAILURE);
+  }
+
+  close(STDIN_FILENO);
+  close(STDOUT_FILENO);
+  close(STDERR_FILENO);
+
+  while (mysignal)
+  {
+    pid_t child_proc;
+    child_proc = fork();
+
+    time_t n = time(NULL); // getting local time
+    struct tm tm = *localtime(&n);
+    char datestr[50];
+    sprintf(datestr, "%d-%02d-%02d_%02d:%02d:%02d", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
+
+    char lokasidownload[100]; //lokasi download
+    strcpy(lokasidownload, cwd);
+    strcat(lokasidownload, "/");
+    strcat(lokasidownload, datestr);
+
+    char linkdownload[80] = "https://picsum.photos/" ;
+    int picsize = (n % 1000) + 50 ;
+    char num[10] ;
+    sprintf(num, "%d", picsize) ;
+    strcat(linkdownload, num) ;
+
+    char namazip[50];
+    strcpy(namazip, datestr);
+    strcat(namazip, ".zip");
+
+    if(child_proc < 0)
+    {
+      exit(EXIT_FAILURE);
+    }
+
+    if(child_proc == 0) // Download per 5 detik & zip-> child
+    {
+      int i = 0;
+      while(i < 10) //Download per 5 detik
+      {
+          pid_t download;
+          download = fork();
+
+          if(download < 0)
+          {
             exit(EXIT_FAILURE);
-            }
-            if (anjas == 0)
-            {
-            char *argv[] = {"unzip", "Musik_for_Stevany.zip", NULL};
-            execv("/bin/unzip", argv);
-            }
-            else{
-                while ((wait(&status)) > 0);
-                char *argv[] = {"mv", "MUSIK/","Musyik",NULL};
-                execv("/bin/mv", argv);
-            }
-        }
-        // system ("wget --no-check-certificate 'https://drive.google.com/uc?id=1ZG8nRBRPquhYXq_sISdsVcXx5VdEgi-J&export=download' -O Musik_for_Stevany.zip");
-        // system ("unzip Musik_for_Stevany.zip");
-        // char *argv[] = {"mv", "MUSIK/","Musyik",NULL};
-        // execv("/bin/mv", argv);
+          }
+
+          if(download == 0)
+          {
+            char* namafile = format_time();
+            char *wget[] = {"wget", "-q", "-O", namafile, linkdownload, NULL};
+            chdir (lokasidownload);
+            execv("/bin/wget", wget) ;
+          }
+          i++;
+          sleep(5);
+      }
+      chdir(lokasidownload);
+      FILE *fp;
+      fp = fopen("status.txt", "w"); //w -> writE
+      fputs(message, fp);
+      fclose(fp);
+      char directory[200];
+      chdir(cwd);
+      char *zip[6] = {"zip", "-m", "-r", namazip, datestr, NULL};
+      execvp("zip", zip);
+    sleep(1);
     }
-    else if (n1 == 0 && n2 > 0 && seconds < 0){
-        while ((wait(&status)) > 0);
+    else // make new folder stiap 30s -> parent
+    {
 
-        pid_t anjoy;
-        anjoy = fork();
+      pid_t makeDir;
+        makeDir = fork();
 
-        if (anjoy < 0)
+        if(makeDir < 0)
         {
-        exit(EXIT_FAILURE);
+          exit(EXIT_FAILURE);
         }
-        if (anjoy == 0)
-        {
-        char *argv[] = {"wget", "--no-check-certificate", "https://drive.google.com/uc?id=1FsrAzb9B5ixooGUs0dGiBr-rC7TS9wTD&export=download", "-O", "Photo_for_Stevany.zip", NULL};
-        execv("/bin/wget", argv);
-        }
-        else{
-            while ((wait(&status)) > 0);
-            
-            pid_t anjiy;
-            anjiy = fork();
 
-            if (anjiy < 0)
-            {
-            exit(EXIT_FAILURE);
-            }
-            if (anjiy == 0)
-            {
-            char *argv[] = {"unzip", "Photo_for_Stevany.zip", NULL};
-            execv("/bin/unzip", argv);
-            }
-            else{
-                while ((wait(&status)) > 0);
-                char *argv[] = {"mv", "FOTO/","Pyoto",NULL};
-                execv("/bin/mv", argv);
-            }
+        if(makeDir == 0)
+        {
+          char *mkdir[3] = {"mkdir", lokasidownload, NULL};
+
+          execv("/bin/mkdir", mkdir);
         }
-        // while ((wait(&status)) > 0);
-        // system ("wget --no-check-certificate 'https://drive.google.com/uc?id=1FsrAzb9B5ixooGUs0dGiBr-rC7TS9wTD&export=download' -O Photo_for_Stevany.zip");
-        // system ("unzip Photo_for_Stevany.zip");
-        // char *argv[] = {"mv", "FOTO/","Pyoto",NULL};
-        // execv("/bin/mv", argv);
+      
+      sleep(40);
     }
-    else if (n1 > 0 && n2 == 0 && seconds < 0){
-        while ((wait(&status)) > 0);
-
-        pid_t anjuy;
-        anjuy = fork();
-
-        if (anjuy < 0)
-        {
-        exit(EXIT_FAILURE);
-        }
-        if (anjuy == 0)
-        {
-        char *argv[] = {"wget", "--no-check-certificate", "https://drive.google.com/uc?id=1ktjGgDkL0nNpY-vT7rT7O6ZI47Ke9xcp&export=download", "-O", "Film_for_Stevany.zip", NULL};
-        execv("/bin/wget", argv);
-        }
-        else{
-            while ((wait(&status)) > 0);
-            
-            pid_t anjey;
-            anjey = fork();
-
-            if (anjey < 0)
-            {
-            exit(EXIT_FAILURE);
-            }
-            if (anjey == 0)
-            {
-            char *argv[] = {"unzip", "Film_for_Stevany.zip", NULL};
-            execv("/bin/unzip", argv);
-            }
-            else{
-                while ((wait(&status)) > 0);
-                char *argv[] = {"mv", "FILM/","Fylm",NULL};
-                execv("/bin/mv", argv);
-            }
-        }
-        // system ("wget --no-check-certificate 'https://drive.google.com/uc?id=1ktjGgDkL0nNpY-vT7rT7O6ZI47Ke9xcp&export=download' -O Film_for_Stevany.zip");
-        // system ("unzip Film_for_Stevany.zip");
-        // char *argv[] = {"mv", "FILM/","Fylm",NULL};
-        // execv("/bin/mv", argv);
-    }
-    //else if (n1>0 && n2>0 && seconds2 > 0){
-    else{
-        while ((wait(&status)) > 0);
-
-        pid_t anjay;
-        anjay = fork();
-
-        if (anjay < 0)
-        {
-        exit(EXIT_FAILURE);
-        }
-        if (anjay == 0)
-        {
-        char *argv[] = {"zip", "-r" ,"Lopyu_Stevany.zip","Musyik/","Pyoto/", "Fylm", NULL};
-        execv("/bin/zip", argv);
-        }
-        // system ("zip -r Lopyu_Stevany.zip Musyik Pyoto Fylm");
-        //rm -r Fylm/ Musyik/ Pyoto/
-        // char *argv[] = {"rm", "-r" ,"Fylm/","Musyik/","Pyoto/",NULL};
-        // execv("/bin/rm", argv);
-        else{
-            while ((wait(&status)) > 0);
-            delete();
-        }
-    }
+  }
 }
