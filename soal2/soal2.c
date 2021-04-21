1e32628883fe2a963d8ed34b0226d2b1de3d59e7
ntsyaabygaill <65441910+ntsyaabygaill@users.noreply.github.com>
2021-04-21 16:52:58 +0700

    Update soal2.c

--- soal2/soal2.c
+++ soal2/soal2.c
@@ -1,45 +1,260 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <sys/types.h>
-#include <sys/stat.h>
+#include <unistd.h>
 #include <string.h>
+#include <dirent.h>
 #include <wait.h>
 #include <time.h>
-#include <memory.h>
-#include <errno.h>
 
-int main(){
-    int n1 = fork();
-    int n2 = fork();
-    int status;
-    if (n1 == 0 && n2 == 0){
-        char *argv[] = {"unzip", "pets.zip", "-d","petshop", NULL};
-        execv("/bin/unzip", argv);
-        //system("unzip pets.zip -d pet");
+void ganti(char *lokasi, char *file, char *nama);
+void pindah(char *lokasi, char *file, char *nama)
+{
+  int status;
+
+  pid_t child2 = fork();
+  if (child2 == 0)
+  {
+    char *argv[] = {"mkdir", "-p", lokasi, NULL};
+    execv("/bin/mkdir", argv);
+  }
+  while ((waitpid(child2, &status, 0)) > 0)
+    ;
+  pid_t child3 = fork();
+
+  if (child3 == 0)
+  {
+    char nick[100];
+    char petname[100];
+    char umur[100];
+    char ket[100];
+
+    strcpy(nick, nama);
+    char delim[] = ";";
+    char *ptr = strtok(nick, delim);
+    int id = 1;
+
+    while (ptr != NULL)
+    {
+      if (id == 2)
+      {
+        sprintf(petname, "%s", ptr);
+      }
+      if (id == 3)
+      {
+        sprintf(umur, "%s", ptr);
+        strtok(umur, "_");
+        strtok(umur, "j");
+        if (umur[strlen(umur) - 1] == '.')
+        {
+          umur[strlen(umur) - 1] = '\0';
+        }
+      }
+      ptr = strtok(NULL, delim);
+      id++;
     }
-    else if (n1==0 && n2>0){
-        while ((wait(&status)) > 0);
-        chdir ("petshop");
-        pid_t child_id;
-        child_id = fork();
-        if (child_id < 0) {
-            exit(EXIT_FAILURE); // Jika gagal membuat proses baru, program akan berhenti
+
+    char loc[100];
+    strcpy(ket, lokasi);
+    strcpy(loc, lokasi);
+    strcat(loc, "/");
+    strcat(loc, petname);
+    strcat(loc, ".jpg");
+
+    FILE *fptr;
+    char st[100];
+    char fname[50];
+    strcpy(fname, lokasi);
+    strcat(fname, "/keterangan.txt");
+    fptr = fopen(fname, "a+");
+
+    fprintf(fptr, "nama : %s\n", petname);
+    fprintf(fptr, "umur : %s tahun\n\n", umur);
+
+    fclose(fptr);
+
+    char *argv[] = {"mv", file, loc, NULL};
+    execv("/bin/mv", argv);
+  }
+}
+void copy(char *src, char *dest, char *lokasi, char *nama)
+{
+  int status;
+  pid_t child = fork();
+  if (child == 0)
+  {
+    char *argv[] = {"cp", src, dest, NULL};
+    execv("/bin/cp", argv);
+  }
+  // copy(src, dest, src2, file2);
+  //pindah(src2, dest, file2);
+  //void pindah(char *lokasi, char *file, char *nama)
+  while ((waitpid(child, &status, 0)) > 0)
+    ;
+  pid_t child2 = fork();
+  if (child2 == 0)
+  {
+    char *argv[] = {"mkdir", "-p", lokasi, NULL};
+    execv("/bin/mkdir", argv);
+  }
+  while ((waitpid(child2, &status, 0)) > 0)
+    ;
+  pid_t child3 = fork();
+
+  if (child3 == 0)
+  {
+    char nick[100];
+    char petname[100];
+    char umur[100];
+    char ket[100];
+
+    strcpy(nick, nama);
+    char delim[] = ";";
+    char *ptr = strtok(nick, delim);
+    int id = 1;
+
+    while (ptr != NULL)
+    {
+      if (id == 2)
+      {
+        sprintf(petname, "%s", ptr);
+      }
+      if (id == 3)
+      {
+        sprintf(umur, "%s", ptr);
+        strtok(umur, "_");
+        strtok(umur, "j");
+        if (umur[strlen(umur) - 1] == '.')
+        {
+          umur[strlen(umur) - 1] = '\0';
         }
-        else if (child_id==0){
-            char *argv[] = {"rm", "-r", "apex_cheats", "musics", "unimportant_files" , NULL};
-            execv("/bin/rm", argv);
-        }
-        else {
-            while ((wait(&status)) > 0);
-            char *argv[] = {"mkdir","-p" ,"cat", "dog", "frog", "guinea", "hamster", "iguana", "ilama", "otter", "parrot", "rabbit", "racoon", "sheep", "tiger", "betta", NULL};
-            execv ("bin/mkdir", argv); 
-            //system ("mkdir cat dog frog guinea hamster iguana ilama otter parrot rabbit racoon sheep tiger betta");
-        }
-        //char *argv[] = {"mkdir", "cat", "dog", "frog", "guinea", "hamster", "iguana", "ilama", "otter", "parrot", "rabbit", "racoon", "sheep", "tiger", "betta", NULL};
-        //execvp("bin/mkdir", argv);
-        //system ("rm -r apex_cheats musics unimportant_files");
-        //system ("mkdir cat dog frog guinea hamster iguana ilama otter parrot rabbit racoon sheep tiger betta");
+      }
+      ptr = strtok(NULL, delim);
+      id++;
     }
-    //while(wait(NULL) !=-1 || errno!=ECHILD);
+
+    char loc[100];
+    strcpy(ket, lokasi);
+    strcpy(loc, lokasi);
+    strcat(loc, "/");
+    strcat(loc, petname);
+    strcat(loc, ".jpg");
+
+    FILE *fptr;
+    char st[100];
+    char fname[50];
+    strcpy(fname, lokasi);
+    strcat(fname, "/keterangan.txt");
+    fptr = fopen(fname, "a+");
+
+    fprintf(fptr, "nama : %s\n", petname);
+    fprintf(fptr, "umur : %s tahun\n\n", umur);
+
+    fclose(fptr);
+
+    char *argv[] = {"mv", dest, loc, NULL};
+    execv("/bin/mv", argv);
+  }
+}
+
+int main()
+{
+  pid_t child, child1, child2, child3, child4, child5, child6;
+  int status;
+  child = fork();
+  if (child == 0)
+  {
+    char *argv[] = {"unzip", "/home/daniel/Documents/shift2/soal2/pets.zip", "-d",
+                    "/home/daniel/Documents/shift2/soal2/petshop", NULL};
+    execv("/usr/bin/unzip", argv);
+  }
+  while ((waitpid(child, &status, 0)) > 0)
+    ;
+  child1 = fork();
+  if (child1 == 0)
+  {
+    char *argv[] = {"find", "/home/daniel/Documents/shift2/soal2/petshop", "-mindepth", "1", "-type", "d", "-exec", "rm",
+                    "-r", "{}", "+", NULL};
+    execv("/usr/bin/find", argv);
+  }
+
+  while ((waitpid(child1, &status, 0)) > 0)
+    ;
+  child2 = fork();
+  if (child2 == 0)
+  {
+    DIR *dp;
+    struct dirent *ep;
+    char path[] = {"/home/daniel/Documents/shift2/soal2/petshop"};
+
+    dp = opendir(path);
+
+    if (dp != NULL)
+    {
+      while ((ep = readdir(dp)))
+      {
+        //puts(ep->d_name);
+
+        char str[100];
+
+        char new[100];
+        memset(str, 0, sizeof(str));
+        memset(new, 0, sizeof(new));
+        strcpy(str, ep->d_name);
+
+        if (ep->d_name[0] != '.')
+        {
+          int i = 0;
+
+          while (str[i] != ';')
+          {
+            new[i] = str[i];
+            i++;
+            if (str[i] == ';')
+            {
+              break;
+            }
+          }
+          i = 0;
+          char lokasi[100] = {"/home/daniel/Documents/shift2/soal2/petshop/"};
+          char file[100] = {"/home/daniel/Documents/shift2/soal2/petshop/"};
+
+          strcat(lokasi, new);
+          strcat(file, ep->d_name);
+          if (strchr(str, '_') != NULL)
+          {
+
+            char file[100];
+            strcpy(file, str);
+            char *file2 = strtok(file, "_");
+            file2 = strtok(NULL, "_");
+            char fol2[100];
+            strcpy(fol2, file2);
+            char *folder2 = strtok(fol2, ";");
+
+            char src[100] = {"/home/daniel/Documents/shift2/soal2/petshop/"};
+            char src2[100] = {"/home/daniel/Documents/shift2/soal2/petshop/"};
+            char dest[100] = {"/home/daniel/Documents/shift2/soal2/petshop/"};
+            strcat(src, str);
+            strcat(dest, file2);
+            strcat(src2, folder2);
+            //src=/home/juned/modul2/petshop/cat;joni;6_dog;daisy;9.jpg
+            //dest=/home/juned/modul2/petshop/
+            //src2=/home/juned/modul2/petshop/dog
+            //file2=dog;daisy;9.jpg
+            copy(src, dest, src2, file2);
+            //pindah(src2, dest, file2);
+          }
+          //lokasi=/home/juned/modul2/petshop/cat
+          //src=/home/juned/modul2/petshop/cat;joni;6.jpg
+          //d_name=cat;joni;6.jpg
+          pindah(lokasi, file, ep->d_name);
+        }
+      }
+
+      (void)closedir(dp);
+    }
+    else
+      perror("Couldn't open the directory");
+  }
 }
